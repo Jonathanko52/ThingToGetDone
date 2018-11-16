4d5a1e7ac2fc6cd335fc435ba15105253c3e3a1a
Jonathan Ko <jonathanko@Jonathans-MacBook-Pro.local>
2018-11-16 14:21:36 -0800

    implementing redux

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import reducers from './reducers.js'
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
 import App from './components/app';
 
+const store = createStore(reducers, composeWithDevTools(applyMiddleware(thunk)))
+
 ReactDOM.render(
-    <App />,
+    <Provider store={store}>
+    <App />
+    </Provider>,
     document.getElementById('root')
 );
